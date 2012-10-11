3e9a195000a4129d73aa7928126ebe07a5d1a425
James Dean <jamesdeanp51@gmail.com>
2012-10-11 13:03:09 +0800

    third commit

---
+++ app/controllers/stretchs_controller.rb
@@ -0,0 +1,8 @@
+class StretchsController < ApplicationController
+  respond_to :html
+  def index
+    respond_to do |format|
+      format.html
+    end
+  end
+end
---
+++ test/unit/helpers/stretchs_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class StretchsHelperTest < ActionView::TestCase
+end
